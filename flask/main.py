9ffd505b4a101ada608c58a92e25fd3d3b5c913b
Brian Tran <briantra23@gmail.com>
2025-03-29 14:59:29 -0400

    added get_activities

--- flask/main.py
+++ flask/main.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from flask_cors import CORS
+import datetime 
 
 app = Flask(__name__)
 CORS(app)
@@ -18,7 +19,33 @@ def get_messages():
     docs = collection.get()
     messages = [doc.to_dict() for doc in docs]
     return {"messages": messages}
+
+@app.route('/create-activity', methods=['POST'])
+def get_activities():
+    data = request.json
+    activity = {
+        "Publisher": data.get("Publisher"),
+        "Title": data.get("Title"),
+        "Description": data.get("Description"),
+        "CreatedDate": datetime.utcnow(),
+        "StartDate": datetime.strptime(data["StartDate"], "%Y-%m-%dT%H:%M:%S") if "StartDate" in data else None,
+        "EndDate": datetime.strptime(data["EndDate"], "%Y-%m-%dT%H:%M:%S") if "EndDate" in data else None,
+        "Latitude": float(data["Latitude"]) if "Latitude" in data else None,
+        "Longitude": float(data["Longitude"]) if "Longitude" in data else None,
+        "Organization": data.get("Organization"),
+        "Categories": data.get("Categories", []),
+        "Participants": data.get("Participants", []),
+        "Limit": data.get("Limit"),
+        "ContactEmail": data.get("ContactEmail"),
+        "Upvotes": int(data.get("Upvotes", 0)),
+        "Downvotes": int(data.get("Downvotes", 0)),
+        "OrganizationOnly": bool(data.get("OrganizationOnly", False))
+    }
     
+    doc_ref = collection.add(activity)
+    doc = doc_ref[1].get().to_dict() 
+    
+    return {"activity": doc}, 201 
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True, port=5000)
