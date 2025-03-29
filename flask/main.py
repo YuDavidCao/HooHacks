35184be21b7222eeece3a06b1476a39cf876998b
Xu Ziqi <xuziqi050501@gmail.com>
2025-03-29 16:50:55 -0400

    do get_user, so we can see the users we manually imported on firebase through the url

--- flask/main.py
+++ flask/main.py
@@ -13,6 +13,7 @@ cred = credentials.Certificate("key.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 collection = db.collection("message")
+users_collection = db.collection("users")
 
 @app.route('/get-messages', methods=['GET'])
 def get_messages():
@@ -46,6 +47,20 @@ def get_activities():
     doc = doc_ref[1].get().to_dict() 
     
     return {"activity": doc}, 201 
+
+
+@app.route('/get-users', methods=['GET'])
+def get_users():
+    #docs = users_collection.document(uid).get()
+    #return docs.to_dict()
+    #doc = users_collection.document(uid).get()
+    #if doc.exists:
+    #    return jsonify(doc.to_dict()), 200
+    #else:
+    #    return jsonify({'error': 'User not found'}), 404
+    docs = users_collection.stream()
+    users = [doc.to_dict() for doc in docs]
+    return jsonify({"users": users}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True, port=5000)
